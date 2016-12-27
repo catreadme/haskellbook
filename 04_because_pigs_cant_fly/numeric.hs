96c33530c02e4307249c21d8c85d5552362bd9eb
Robin Elvedi <trivialic@posteo.net>
2016-12-27 16:37:39 +0100

    numeric for ch04

---
+++ 04_because_pigs_cant_fly/numeric.hs
@@ -0,0 +1,26 @@
+module Numeric where
+
+{-
+  Integral numbers
+    - whole
+    - positive and negative
+
+  Int:     fixed-precision numbers with a lower and upper limit
+
+  Integer: arbitrarily large or small numbers
+-}
+
+{-
+  Fractional numbers
+    - non whole
+
+  Float:      single-precision floating point numbers
+
+  Double:     double-precision floating point numbers with twice as many bits as
+              Float numbers
+
+  Rational:   fractional number that represents a ratio of two Integers
+
+  Scientific: space efficient almost-arbitrary precision scientific number
+              provided by the scientific package
+-}
